2d32e4156fc88c0edeb01318c8422313429bec4d
monasubedi <sbd.mona@gmail.com>
2024-06-14 06:52:05 -0400

    add the components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Box } from '@mui/material';
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './components';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-   
-    </div>
-  );
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: '#000' }}>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact element={<Feed />} />
+          <Route path='/video/:id' element={<VideoDetail />} />
+          <Route path='/channel/:id' element={<ChannelDetail />} />
+          <Route path='/search/:searchTerm' exact element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  )
 }
 
 export default App;
