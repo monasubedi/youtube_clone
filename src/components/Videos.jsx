de7bb35544d8694dcc92e45f16d6a3593e3dcb2b
monasubedi <sbd.mona@gmail.com>
2024-06-17 09:07:57 -0400

    add update

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -3,9 +3,10 @@ import React from "react";
 import ChannelCard from "./ChannelCard";
 import VideoCard from "./VideoCard";
 
-const Videos = ({ videos }) => {
+const Videos = ({ videos, direction }) => {
+  if (!videos?.length > 0) return "Loading...";
   return (
-    <Stack direction={"row"} flexWrap={"wrap"} gap={2}>
+    <Stack direction={direction || "row"} flexWrap={"wrap"} gap={2}>
       {videos.map((item, idx) => (
         <Box key={idx}>
           {item.id.channelId && <ChannelCard channelDetail={item} />}
